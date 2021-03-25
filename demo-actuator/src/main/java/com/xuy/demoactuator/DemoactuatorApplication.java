32ec76b4902ad28ed0e190b7f2c542980448a792
ywltxz <1061778273@qq.com>
2021-03-25 11:22:08 +0800

    .

---
+++ demo-actuator/src/main/java/com/xuy/demoactuator/DemoactuatorApplication.java
@@ -0,0 +1,13 @@
+package com.xuy.demoactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoactuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoactuatorApplication.class, args);
+    }
+
+}
---
+++ demo-actuator/src/test/java/com/xuy/demoactuator/DemoactuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuy.demoactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoactuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
