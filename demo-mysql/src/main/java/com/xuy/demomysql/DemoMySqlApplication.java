a4d3799c3140293348e5d2407c004f006bf92ea1
ywltxz <1061778273@qq.com>
2021-04-01 15:57:56 +0800

    .

---
+++ demo-mysql/src/main/java/com/xuy/demomysql/DemoMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.xuy.demomysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMySqlApplication.class, args);
+    }
+
+}
---
+++ demo-mysql/src/test/java/com/xuy/demomysql/DemoMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuy.demomysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
